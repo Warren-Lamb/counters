0c6cb16f4a5d4012c308300f28fd937fed83768e
Warren-Lamb <10warren10@gmail.com>
2019-05-16 19:33:41 -0400

    added update fn, view bttn

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,9 @@
 module Main exposing (main)
 
 import Browser exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
 
@@ -29,3 +32,52 @@ type Msg
     | Increment Int
     | Decrement Int
     | DeleteCounter Int
+
+
+
+-- Update
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NewCounter ->
+            model ++ [ 0 ]
+
+        Increment index ->
+            List.indexedMap
+                (\idx count ->
+                    if idx == index then
+                        count + 1
+
+                    else
+                        count
+                )
+                model
+
+        Decrement index ->
+            List.indexedMap
+                (\idx count ->
+                    if idx == index then
+                        count - 1
+
+                    else
+                        count
+                )
+                model
+
+        DeleteCounter index ->
+            let
+                front =
+                    List.take index model
+
+                back =
+                    List.drop (index + 1) model
+            in
+            front ++ back
+
+-- View
+
+view : Model -> Html Msg
+view model =
+    button [onClick NewCounter] [text "New counter"]
